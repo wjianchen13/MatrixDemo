c145fe3fc7b8da73b4be6192ce46a89c49b081ae
cwj <282330332@qq.com>
2025-12-10 12:18:29 +0800

    添加基础测试页面

---
+++ app/src/main/java/com/example/matrixdemo/MyApplication.java
@@ -0,0 +1,22 @@
+package com.example.matrixdemo;
+
+import android.app.Application;
+import android.content.Context;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
--- app/src/main/java/com/example/matrixdemo/test1/TestActivity1.java
+++ app/src/main/java/com/example/matrixdemo/test1/TestActivity1.java
@@ -1,4 +1,4 @@
-package com.example.matrixdemo;
+package com.example.matrixdemo.test1;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,15 +10,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.matrixdemo.test1.TestActivity1;
+import com.example.matrixdemo.R;
 
-public class MainActivity extends AppCompatActivity {
+public class TestActivity1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test1);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -29,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
     public void onTest1(View v) {
         startActivity(new Intent(this, TestActivity1.class));
     }
-
 
 
 }
